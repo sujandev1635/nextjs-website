7bda34b4948816fa21c6a3e6680296df9b0a8e29
sujandev1635 <sujandev1635@gmail.com>
2023-06-01 01:54:51 +0600

    navbar completed

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <div>2023 www.sujandev.xyz All rights reserved.</div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Footer
